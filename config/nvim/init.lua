9b2e7312d7a750108d7c18ba28a976770c6fbe8f
lcensies <lcensies@proton.me>
2024-02-23 16:41:07 +0300

    nvim temp commit

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -24,3 +24,10 @@ require "plugins"
 vim.g.dap_virtual_text = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- Figure out the system Python for Neovim.
+-- if vim.fn.exists "$VIRTUAL_ENV" == 1 then
+--   vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system "which -a python3 | head -n2 | tail -n1", "\n", "", "g")
+-- else
+--   vim.g.python3_host_prog = vim.fn.substitute(vim.fn.system "which python3", "\n", "", "g")
+-- end
